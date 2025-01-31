9ff6bfbd9ebf4f4ab00b4bcd46195d5a2bb22adc
Keith Bauson <kwbauson@gmail.com>
2025-01-31 16:28:40 -0500

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -4,11 +4,11 @@
   outputs = { self, nixpkgs, home-manager, flake-utils }: flake-utils.lib.eachDefaultSystem (system: {
     packages =
       let
-        inherit (nixpkgs.legacyPackages.${system}.writers) writeBashBin;
+        pkgs = nixpkgs.legacyPackages.${system};
       in
       {
         homeConfigurations.default = home-manager.lib.homeManagerConfiguration {
-          inherit system;
+          inherit pkgs;
           username = "keith"; # UPDATE
           homeDirectory = "/home/keith"; # UPDATE
           configuration = { pkgs, ... }: {
@@ -16,13 +16,13 @@
           };
         };
 
-        hm = writeBashBin "hm" ''
+        hm = writers.writeBashBin "hm" ''
           ${home-manager.packages.${system}.home-manager}/bin/home-manager \
             --flake ${self}#default \
             "$@"
         '';
-        build = writeBashBin "build" "nix run ${self}#hm build";
-        switch = writeBashBin "switch" "nix run ${self}#hm switch";
+        build = writers.writeBashBin "build" "nix run ${self}#hm build";
+        switch = writers.writeBashBin "switch" "nix run ${self}#hm switch";
       };
   });
 }
