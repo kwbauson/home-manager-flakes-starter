797ef29af343e59cb45c120736febc5b62b3ada0
Keith Bauson <kwbauson@gmail.com>
2025-01-31 16:48:49 -0500

    Automatic update (#60)
    * Automatic update
    
    * Update flake.nix
    
    * Update flake.nix
    
    ---------
    
    Co-authored-by: kwbauson <kwbauson@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -6,15 +6,17 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
         inherit (pkgs.writers) writeBashBin;
+        hmModule = { config, pkgs, ... }: {
+          home.username = "keith"; # UPDATE
+          home.homeDirectory = "/home/${config.home.username}"; # UPDATE if home directory different from username
+          home.stateVersion = "24.11"; # UPDATE to the latest by searching for "stateVersion" in the docs https://nix-community.github.io/home-manager/
+          home.packages = with pkgs; [ hello ];
+        };
       in
       {
         homeConfigurations.default = home-manager.lib.homeManagerConfiguration {
-          inherit system pkgs;
-          username = "keith"; # UPDATE
-          homeDirectory = "/home/keith"; # UPDATE
-          configuration = { pkgs, ... }: {
-            home.packages = with pkgs; [ hello ];
-          };
+          inherit pkgs;
+          modules = [ hmModule ];
         };
 
         hm = writeBashBin "hm" ''
